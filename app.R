0f8fa7faab3d8035c9e0115dbc9dca9c9e077d41
Louise De Neyer <138357257+Wonderia1@users.noreply.github.com>
2026-02-11 16:27:38 +0100

    Add reset button

--- app.R
+++ app.R
@@ -35,8 +35,15 @@ ui <- page_sidebar(
       min = min(heart$AGE),
       max = max(heart$AGE),
       value = c(min(heart$AGE), max(heart$AGE))
+    ),
+    actionButton(
+      inputId = "reset",
+      label = "Reset",
+      icon = bsicons::bs_icon("arrow-counterclockwise")
     )
   ),
+  
+  
   navset_tab(
     nav_panel("Overview", 
       layout_column_wrap(
@@ -123,7 +130,16 @@ server <- function(input, output, session) {
     ggplotly(p)
   })
   
-  browser()
+  observeEvent(input$reset, {
+    updateSelectInput(session, "outcome", selected = "All")
+    updateSelectInput(session, "diagnosis", selected = "All")
+    updateSelectInput(session, "drg", selected = "All")
+    updateSliderInput(session, "age_range",
+                      value = c(min(heart$AGE), max(heart$AGE)))
+  })
+  
+  
+  
   output$data_table <- DT::renderDataTable({
     filtered_data()
   })
